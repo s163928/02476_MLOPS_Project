d7df6e3b69fb21687ffa3e63c4c40d7cf6b591bb
madscort <madsn@msn.com>
2023-01-09 09:00:38 +0100

    Added script for loading data with lightning data module

--- src/models/train_LN_model.py
+++ src/models/train_LN_model.py
@@ -1,21 +1,15 @@
 import pytorch_lightning as pl
 import torchvision
 from src.models.LN_model import LN_model
+from src.data.LN_data_module import Flowers102DataModule
 from torch.utils.data import DataLoader
 import torchvision.transforms as tt
-
-transform = tt.Compose([
-    tt.Resize((128,128)),
-    tt.ToTensor()
-])
 
 def main():
     print("Training day and night")
 
     model = LN_model()
-
-    train_set = torchvision.datasets.Flowers102('./data/raw', transform=transform)
-    train_loader = DataLoader(train_set, batch_size=68, shuffle=True)
+    data = Flowers102DataModule()
 
     trainer = pl.Trainer(
         limit_train_batches=0.20, # Limit to 20% of total size.
@@ -24,7 +18,7 @@ def main():
         log_every_n_steps=1)
     trainer.fit(
         model=model,
-        train_dataloaders=train_loader)
+        datamodule=data)
 
 
 if __name__ == "__main__":
