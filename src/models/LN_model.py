93f6c812cbb5f7a07207c9b2d4ec04a5de8bd1c8
Gowrisankar <gowrisankar2396@gmail.com>
2023-01-18 14:23:08 +0100

    Adding HyperParameters to log

--- src/models/LN_model.py
+++ src/models/LN_model.py
@@ -1,21 +1,27 @@
+import torch.nn.functional as F
+from torch import nn, optim, utils, Tensor
+from torchmetrics.functional import accuracy
 import pytorch_lightning as pl
+import wandb
 import timm
-from torch import nn, optim
-
 
 class LN_model(pl.LightningModule):
-    def __init__(
-        self, model_name="resnet18", pretrained=True, in_chans=3, num_classes=102
-    ):
+    def __init__(self, model_name='resnet18', pretrained=True, 
+    in_chans=3, num_classes=102,lr=1e-3, loss='CrossEntropyLoss'):
         super().__init__()
         self.model = timm.create_model(
             model_name=model_name,
             pretrained=pretrained,
             in_chans=in_chans,
-            num_classes=num_classes,
-        )
-        self.criterion = nn.CrossEntropyLoss()
+            num_classes=num_classes)
 
+        # optimizer parameters
+        self.lr = lr
+        self.criterion = nn.CrossEntropyLoss() #getattr(nn,loss) 
+
+        # save hyper-parameters to self.hparams (auto-logged by W&B)
+        self.save_hyperparameters()
+        
     def forward(self, x):
         x = self.model(x)
         return x
@@ -24,18 +30,22 @@ class LN_model(pl.LightningModule):
         data, target = batch
         preds = self(data)
         loss = self.criterion(preds, target)
+        acc = accuracy(preds, target)
         self.log("train_loss", loss)
+        self.log("train_accuracy", acc)
         return loss
 
     def validation_step(self, batch, batch_idx):
         data, target = batch
         preds = self(data)
         val_loss = self.criterion(preds, target)
+        val_acc = accuracy(preds, target)
         self.log("val_loss", val_loss)
+        self.log("train_accuracy", val_acc)
 
     def predict_step(self, batch, batch_idx):
         preds = self(batch)
         return preds.argmax(dim=-1)
 
     def configure_optimizers(self):
-        return optim.Adam(self.parameters(), lr=1e-3)
+        return optim.Adam(self.parameters(), lr=self.lr)
