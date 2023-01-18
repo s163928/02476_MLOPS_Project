92dc848afc547578263d8a264af21306cb1d2954
Gowrisankar <gowrisankar2396@gmail.com>
2023-01-18 14:23:08 +0100

    Adding HyperParameters to log

--- src/models/LN_model.py
+++ src/models/LN_model.py
@@ -1,10 +1,9 @@
 import torch.nn.functional as F
 from torch import nn, optim, utils, Tensor
-# from torchmetrics.functional import accuracy
+from torchmetrics.functional import accuracy
 import pytorch_lightning as pl
+import wandb
 import timm
-from torch import nn, optim
-
 
 class LN_model(pl.LightningModule):
     def __init__(self, model_name='resnet18', pretrained=True, 
@@ -15,14 +14,13 @@ class LN_model(pl.LightningModule):
             pretrained=pretrained,
             in_chans=in_chans,
             num_classes=num_classes)
-        self.criterion = nn.CrossEntropyLoss() 
 
         # optimizer parameters
         self.lr = lr
+        self.criterion = nn.CrossEntropyLoss() #getattr(nn,loss) 
 
         # save hyper-parameters to self.hparams (auto-logged by W&B)
         self.save_hyperparameters()
-
         
     def forward(self, x):
         x = self.model(x)
@@ -32,18 +30,18 @@ class LN_model(pl.LightningModule):
         data, target = batch
         preds = self(data)
         loss = self.criterion(preds, target)
-        # acc = accuracy(preds, target)
+        acc = accuracy(preds, target)
         self.log("train_loss", loss)
-        # self.log("train_accuracy", acc)
+        self.log("train_accuracy", acc)
         return loss
 
     def validation_step(self, batch, batch_idx):
         data, target = batch
         preds = self(data)
         val_loss = self.criterion(preds, target)
-        # val_acc = accuracy(preds, target)
+        val_acc = accuracy(preds, target)
         self.log("val_loss", val_loss)
-        # self.log("train_accuracy", val_acc)
+        self.log("train_accuracy", val_acc)
 
     def predict_step(self, batch, batch_idx):
         preds = self(batch)
