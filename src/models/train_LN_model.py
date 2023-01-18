657891519c3b0bd3e35e9c3dd2c371e6959e8d73
Gowrisankar <gowrisankar2396@gmail.com>
2023-01-18 12:03:18 +0100

    Removed Adding Secrets in Docker Build Steps

--- src/models/train_LN_model.py
+++ src/models/train_LN_model.py
@@ -2,7 +2,9 @@ import pytorch_lightning as pl
 from src.models.LN_model import LN_model
 from src.data.LN_data_module import Flowers102DataModule
 from pytorch_lightning.callbacks import EarlyStopping, ModelCheckpoint
+from google.cloud import storage
 import shutil
+import os
 
 def main():
     print("Training day and night")
@@ -29,12 +31,33 @@ def main():
         model=model,
         datamodule=data)
 
+def upload_model(model_name = 'model.ckpt',
+    model_path = "/models"
+    bucket_name = "/gcs/mlops-project/jobs/training/vertex-with-docker"):
+
+    # Create a new client
+    storage_client = storage.Client()
+
+    # Set the name of the new bucket
+    bucket_name = bucket_name
+
+    # Create the new bucket
+    bucket = storage_client.create_bucket(bucket_name)
+    print("Bucket {} created.".format(bucket.name))
+
+    # Upload a file to the new bucket
+    blob = bucket.blob(model_name)
+    blob.upload_from_filename(os.path.join(model_path, model_name))
+    print("File uploaded to {}.".format(blob.name))
+
+
 if __name__ == "__main__":
     main()
 
     # copy model file after training to gcp-bucket
-    shutil
-    gcp_bucket = '/gcs/mlops-project/jobs/vertex-with-docker'
-    model_dir = '/models'
-    shutil.copytree(model_dir, gcp_bucket)
-    print("Model saved to GCP Bucket")
+    # gcp_bucket = '/gcs/mlops-project/jobs/vertex-with-docker'
+    # model_dir = '/models'
+    # shutil.copytree(model_dir, gcp_bucket)
+    # print("Model saved to GCP Bucket")
+
+
