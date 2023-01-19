513ef6374b8fc8898d06f067f6774ecc7e2c895a
Gowrisankar <gowrisankar2396@gmail.com>
2023-01-19 18:07:24 +0100

    Removed duplicate gcp upload

--- src/models/train_LN_model.py
+++ src/models/train_LN_model.py
@@ -124,41 +124,5 @@ def upload_model(model_name = 'model.ckpt',
         shutil.copy2(os.path.join(model_dir,model_name), gcp_bucket)
         print("Model saved to GCP Bucket")
 
-    upload_model()
-
-def upload_model(model_name = 'model.ckpt',
-    model_path = "./models",
-    bucket_name = "/gcs/mlops-project/jobs/training/vertex-with-docker",
-    mode = "vertex-job"):
-
-    if mode!= "vertex-job":
-        from google.cloud import storage
-
-        # Create a new client
-        storage_client = storage.Client()
-
-        # Set the name of the new bucket
-        bucket_name = bucket_name
-
-        try:
-            # Create the new bucket
-            bucket = storage_client.create_bucket(bucket_name)
-            print("Bucket {} created.".format(bucket.name))
-        except Exception as e:
-            print(e)
-            bucket = storage_client.get_bucket(bucket_name)
-            
-        # Upload a file to the new bucket
-        blob = bucket.blob(model_name)
-        blob.upload_from_filename(os.path.join(model_path, model_name))
-        print("File uploaded to {}.".format(blob.name))
-    
-    else:
-        # copy model file after training to gcp-bucket
-        gcp_bucket = '/gcs/mlops-project/jobs/vertex-with-docker'
-        model_dir = '/models'
-        shutil.copy2(os.path.join(model_dir,model_name), gcp_bucket)
-        print("Model saved to GCP Bucket")
-
 if __name__ == "__main__":
     main()
